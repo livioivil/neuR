a197366ccc57f1c075235088762bc1edbece6b04
kharati <eh.kh.ko@gmail.com>
2015-12-05 17:29:33 +0100

    Update select_vxls.R
    In line 54, it seems we need to define the initial value for "tcs" in order to be used in ".get.mean.tcs()" function and "tcs()" function in line 70.

--- R/select_vxls.R
+++ R/select_vxls.R
@@ -51,7 +51,7 @@ pixelize <- function(D,reduce.by=2){
     seq(1,dim(D)[2]-reduce.by[2]+1,by=reduce.by[2]),
     seq(1,dim(D)[3]-reduce.by[3]+1,by=reduce.by[3]))
   
-#   tcs=D@data$tcs
+    tcs=D@data$tcs
 #   i2=10
 #   i3=10
 #   i1=10
